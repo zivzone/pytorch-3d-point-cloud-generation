fa6a7f806e6efb14770655286f7949ba144c631a
Ziv <zhongturtle@gmail.com>
2020-02-02 16:35:11 +0800

    Add files via upload
    feat: update utils.py for part-loss training
    
    update utils.py for part-loss training
    add crossentropyloss

--- utils.py
+++ utils.py
@@ -88,7 +88,8 @@ def unpack_batch_novel(batch, device):
 
 def define_losses():
     l1_loss = nn.L1Loss()
-    bce_loss = nn.BCEWithLogitsLoss()
+    #bce_loss = nn.BCEWithLogitsLoss()
+    bce_loss = nn.CrossEntropyLoss()
     return l1_loss, bce_loss
 
 def build_structure_generator(cfg):
